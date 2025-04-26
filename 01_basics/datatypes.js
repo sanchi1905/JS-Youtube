90c68ad1500ddedff4179c8dd46cf90f7d506e0e
Sanchi Sisodia <sanchisisodia121@gmail.com>
2025-04-26 11:02:48 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treats all JS code as newer version
+
+// alert(3 + 3); // js's emgine is hidden in browser and it is executed in the background and we can't see it here beacause we are using Nodejs not browser
+
+console.log(3 + 3); // this is the way to see the output in Nodejs
+
+let name = "sanchi"
+let age =22
+let isLoggedIn = true
+
+// **PRIMITIVE DATA TYPES**
+//  number => 2 topower 53
+// bigint
+//string => ""
+// boolean => true/false
+// null => standalone value or empty value
+// undefined => no value or not assignned value
+// symbol => unique value
+
+// object
+console.log(typeof age); // number
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
